1d0f558f77036500e1e0eb21fdc6c444e438c61a
aybike <tonn.raus09@gmail.com>
2021-01-03 21:54:25 +0100

    doc(js-css): click event toggle and classes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 const checkbox= document.getElementById('boxcheck');
 const mode=document.getElementById('modus')
+const ball = document.querySelector('.toggle-ball')
 
-checkbox.addEventListener('change',e=>{
-        e.preventDefault();
-        if(e.target.checked){
-                mode.textContent='Dark Mode'
-        }else{
-                document.body.classList.toggle('dark');
+checkbox.addEventListener('click',() => {
+        // debugger
+        if(mode.textContent === 'Dark Mode'){
                 mode.textContent='Light Mode'
+                document.body.classList.toggle('dark');
+                ball.classList.toggle('toggled');
+                checkbox.classList.toggle('colored');
+                return null;
+                
+        }
+        if(mode.textContent === 'Light Mode'){
+                document.body.classList.toggle('dark');
+                mode.textContent='Dark Mode';
+                ball.classList.toggle('toggled');
+                checkbox.classList.toggle('colored');
+                return null;
         }
 });
-
+ 
